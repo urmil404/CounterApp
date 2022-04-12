bf18b68871e6810545b372bfeefd5943cc1f80a0
urmil404 <urmil8989@gmail.com>
2022-04-12 05:31:29 +0530

    counter app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+  
 export class AppComponent {
-  title = '01_Counter_app';
+  title: String = 'Counter App';
+  count: number = 0;
+
+  handleIncrease = () => {
+    this.count = this.count + 1;
+  };
+  handleDecrease = () => {
+    if (this.count > 0) {
+      this.count = this.count - 1;
+    }
+  };
+  handleReset = () => {
+    this.count = 0;
+  };
+
 }
